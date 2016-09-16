399e3bb90e51183228e3188ce2b9b871239ab800
Marcos Paliari <marcos@paliari.com.br>
2016-09-16 09:54:33 -0300

    tests/models/AbstractModel.geEm

--- tests/models/AbstractModel.php
+++ tests/models/AbstractModel.php
@@ -1,12 +1,19 @@
 <?php
-use Paliari\Doctrine\Ransack;
 
 class AbstractModel extends \Paliari\Doctrine\AbstractRansackModel
 {
 
     public static function find($id)
     {
-        return Ransack::getEm()->find(get_called_class(), $id);
+        return static::getEm()->find(get_called_class(), $id);
+    }
+
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    public static function getEm()
+    {
+        return EM::getEm();
     }
 
 }
