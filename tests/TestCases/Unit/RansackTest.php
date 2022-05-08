3ee97fac5ca98c4ecce97dcab78fd1d0b4b60be9
Marcos Paliari <marcos@paliari.com.br>
2022-05-08 18:41:02 -0300

    test: groupBy and orderBy

--- tests/TestCases/Unit/RansackTest.php
+++ tests/TestCases/Unit/RansackTest.php
@@ -3,7 +3,9 @@
 namespace Tests\TestCases\Unit;
 
 use Paliari\Doctrine\Ransack;
+use Paliari\Doctrine\RansackBuilder;
 use Paliari\Doctrine\RansackConfig;
+use Paliari\Doctrine\VO\WhereOrderByVO;
 use Paliari\Doctrine\VO\WhereParamsVO;
 use PHPUnit\Framework\TestCase;
 use Tests\EM;
@@ -11,18 +13,22 @@ use User;
 
 class RansackTest extends TestCase
 {
+    protected Ransack $ransack;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->ransack = new Ransack(new RansackConfig());
+    }
+
     public function testDql()
     {
-        $modelName = User::class;
-        $alias = 't';
-        $ransack = new Ransack(new RansackConfig());
-        $qb = EM::getEm()->createQueryBuilder()->from($modelName, $alias);
         $paramsVO = new WhereParamsVO();
         $paramsVO->where = [
             'person_name_cont' => 'ze das cove',
             'person_address_street_eq' => 'abc',
         ];
-        $rb = $ransack->query($qb, $modelName, $alias)->where($paramsVO)->includes();
+        $rb = $this->newRansackBuilder($paramsVO);
         $dql = $rb->getQueryBuilder()->getDQL();
         $sql = $rb->getQueryBuilder()->getQuery()->getSQL();
         $expectedDql = 'SELECT t FROM User t LEFT JOIN t.person t_person LEFT JOIN t_person.address t_person_address WHERE t_person.name LIKE :t_person_name_cont AND t_person_address.street = :t_person_address_street_eq';
@@ -37,5 +43,40 @@ class RansackTest extends TestCase
             '%ze%das%cove%',
             $rb->getQueryBuilder()->getParameter('t_person_name_cont')->getValue(),
         );
+    }
+
+    public function testOrderBy()
+    {
+        $paramsVO = new WhereParamsVO();
+        $paramsVO->orderBy = [
+            new WhereOrderByVO(['field' => 'person.name', 'order' => 'ASC']),
+            new WhereOrderByVO(['field' => 'person.id', 'order' => 'DESC']),
+        ];
+        $rb = $this->newRansackBuilder($paramsVO);
+        $expectedDql = 'SELECT t FROM User t LEFT JOIN t.person t_person ORDER BY t_person.name ASC, t_person.id DESC';
+        $dql = $rb->getQueryBuilder()->getDQL();
+        $this->assertEquals($expectedDql, $dql);
+    }
+
+    public function testGroupBy()
+    {
+        $paramsVO = new WhereParamsVO();
+        $paramsVO->groupBy = [
+            'person.name',
+            'person.address_id',
+        ];
+        $rb = $this->newRansackBuilder($paramsVO);
+        $expectedDql = 'SELECT t FROM User t LEFT JOIN t.person t_person GROUP BY t_person.name, t_person.address_id';
+        $dql = $rb->getQueryBuilder()->getDQL();
+        $this->assertEquals($expectedDql, $dql);
+    }
+
+    protected function newRansackBuilder(WhereParamsVO $paramsVO): RansackBuilder
+    {
+        $modelName = User::class;
+        $alias = 't';
+        $qb = EM::getEm()->createQueryBuilder()->from($modelName, $alias);
+
+        return $this->ransack->query($qb, $modelName, $alias)->where($paramsVO)->includes();
     }
 }
